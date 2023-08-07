3b02617c060c0acfb94384f0f08d36a497171bbd
usmanliaqat <usmanliaqat562@gmail.com>
2023-08-07 10:43:20 +0500

    changes

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,4 @@
+
 <footer id="footer">
     <div class="container">
         <div class="main-footer pb-3">
@@ -6,9 +7,16 @@
           
            <div class="footer-right-side">
             <h5>Get a quote</h5>
-            <div class="input-type">
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder=" Enter your Email">
-            </div>
+            <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+              <script>
+      hbspt.forms.create({
+     region: "na1",
+       portalId: "39782415",
+      formId: "a4a75ae0-9616-4fc4-9123-70124c296b53"
+  });
+</script>
+
+
            </div>
 </div>  
       <div class="footer-bootom pt-3 ">
@@ -19,7 +27,19 @@
 </footer>
 
 </div>
-
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64d083b9cc26a871b02db352/1h778546l';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
